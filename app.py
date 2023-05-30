9fa47cf6e256232b62b5bca542973076a7d7c50a
Abhinit Patil <abhineet0309@gmail.com>
2023-05-30 18:02:30 -0400

    Now handles user input for stock symbol

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import os
 import dash
-import time
+from dash.dependencies import Input, Output, State
+import openai
 import pandas as pd
 import yfinance as yf
 from prophet import Prophet
@@ -13,94 +15,74 @@ from sklearn.model_selection import train_test_split
 # Load the .env file
 load_dotenv('/Users/abhi/Desktop/projects/python_stock_project/.env')
 
-# Step 1: Gather Data
+# Access the API key
+api_key = os.getenv("OPENAPI_KEY")
+openai.api_key = api_key
 
-# Download historical data as pandas DataFrame
-data = yf.download('AAPL','2020-01-01','2023-12-31')
+# Step 1: Gather Data
+def get_stock_data(ticker):
+    data = yf.download(ticker, '2020-01-01', '2023-12-31')
+    return data
 
 # Step 2: Feature Engineering
-
-data['Rolling'] = data['Close'].rolling(window=5).mean()
+def calculate_rolling_average(data):
+    data['Rolling'] = data['Close'].rolling(window=5).mean()
+    return data
 
 # Step 3: Simple ML model
+def train_linear_regression_model(data):
+    # Drop any rows with missing values
+    data.dropna(inplace=True)
 
-# Drop any rows with missing values
-data.dropna(inplace=True)
+    # The feature is the rolling window
+    X = data[['Rolling']]
 
-# The feature is the rolling window
-X = data[['Rolling']]
+    # The target variable is the next day's closing price
+    y = data['Close'].shift(-1)
 
-# The target variable is the next day's closing price
-y = data['Close'].shift(-1)
+    # Split data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X[:-1], y[:-1], test_size=0.2, random_state=0)
 
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X[:-1], y[:-1], test_size=0.2, random_state=0)
+    # Create and fit the model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
 
-# Create and fit the model
-model = LinearRegression()
-model.fit(X_train, y_train)
+    # Now we can make predictions on the test set
+    predictions = model.predict(X_test)
 
-# Now we can make predictions on the test set
-predictions = model.predict(X_test)
+    return model
 
 # Step 4: Build the dashboard
 app = dash.Dash(__name__)
 
 # Prepare data for Prophet
-prophet_data = data[['Close']].reset_index()
-prophet_data.columns = ['ds', 'y']
+def prepare_prophet_data(data):
+    prophet_data = data[['Close']].reset_index()
+    prophet_data.columns = ['ds', 'y']
+    return prophet_data
 
 # Instantiate a new Prophet object
-m = Prophet(daily_seasonality=True)
-
-# Fit the Prophet model on our data
-m.fit(prophet_data)
+def fit_prophet_model(data):
+    m = Prophet(daily_seasonality=True)
+    m.fit(data)
+    return m
 
 # Create a dataframe for future predictions
-future = m.make_future_dataframe(periods=60)
-
-# Use the model to make predictions
-forecast = m.predict(future)
-
-# Plotly figure setup
-fig = go.Figure()
-
-# Add actual data to the plot
-fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Actual'))
-
-# Add forecast data to the plot
-fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat'], mode='lines', name='Forecast'))
-
-# Add confidence interval
-fig.add_trace(go.Scatter(
-    name='Upper Bound',
-    x=forecast['ds'],
-    y=forecast['yhat_upper'],
-    mode='lines',
-    marker=dict(color="#444"),
-    line=dict(width=0),
-    showlegend=False))
-fig.add_trace(go.Scatter(
-    name='Lower Bound',
-    x=forecast['ds'],
-    y=forecast['yhat_lower'],
-    marker=dict(color="#444"),
-    line=dict(width=0),
-    mode='lines',
-    fillcolor='rgba(68, 68, 68, 0.3)',
-    fill='tonexty',
-    showlegend=False))
+def make_prophet_forecast(model):
+    future = model.make_future_dataframe(periods=60)
+    forecast = model.predict(future)
+    return forecast
 
 # Set plot title and labels
-fig.update_layout(
-    title_text='AAPL Stock Price Forecast',
-    yaxis_title='Stock Price',
-    xaxis_title='Date',
-    hovermode='x'
-)
+def set_plot_layout(fig):
+    fig.update_layout(
+        title_text='Stock Price Forecast',
+        yaxis_title='Stock Price',
+        xaxis_title='Date',
+        hovermode='x'
+    )
 
-# Step 5: Generate Data Summary
-
+# Generate the summary after making the prediction
 def generate_summary(data, prediction):
     message = f"""
     The stock had a closing price of ${round(data['Close'].iloc[-1], 2)} today. 
@@ -109,16 +91,70 @@ def generate_summary(data, prediction):
     """
     return message
 
-# Generate the summary after making the prediction
-summary = generate_summary(data, model.predict([[data['Rolling'].iloc[-1]]])[0])
-
 # Define Dash application layout
 app.layout = html.Div([
-    dcc.Graph(figure=fig),
-    html.Div(children=[
-        html.P(id='summary', children=[generate_summary(data, model.predict([[data['Rolling'].iloc[-1]]])[0])])
-    ], style={'textAlign': 'center'})
+    dcc.Input(id='ticker-input', type='text', value='AAPL', placeholder='Enter stock ticker'),
+    html.Button(id='submit-button', children='Submit', n_clicks=0),
+    dcc.Graph(id='graph'),
+    html.Div(id='summary', style={'textAlign': 'center'})
 ])
+
+@app.callback(
+    [Output('graph', 'figure'), Output('summary', 'children')],
+    [Input('submit-button', 'n_clicks')],
+    [State('ticker-input', 'value')]
+)
+def update_output(n_clicks, ticker):
+    # Gather Data
+    data = get_stock_data(ticker)
+
+    # Feature Engineering
+    data = calculate_rolling_average(data)
+
+    # Simple ML model
+    model = train_linear_regression_model(data)
+
+    # Build the Prophet model
+    prophet_data = prepare_prophet_data(data)
+    m = fit_prophet_model(prophet_data)
+    forecast = make_prophet_forecast(m)
+
+    # Plotly figure setup
+    fig = go.Figure()
+
+    # Add actual data to the plot
+    fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Actual'))
+
+    # Add forecast data to the plot
+    fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat'], mode='lines', name='Forecast'))
+
+    # Add confidence interval
+    fig.add_trace(go.Scatter(
+        name='Upper Bound',
+        x=forecast['ds'],
+        y=forecast['yhat_upper'],
+        mode='lines',
+        marker=dict(color="#444"),
+        line=dict(width=0),
+        showlegend=False))
+    fig.add_trace(go.Scatter(
+        name='Lower Bound',
+        x=forecast['ds'],
+        y=forecast['yhat_lower'],
+        marker=dict(color="#444"),
+        line=dict(width=0),
+        mode='lines',
+        fillcolor='rgba(68, 68, 68, 0.3)',
+        fill='tonexty',
+        showlegend=False))
+
+    # Set plot title and labels
+    set_plot_layout(fig)
+
+    # Generate the summary after making the prediction
+    summary = generate_summary(data, model.predict([[data['Rolling'].iloc[-1]]])[0])
+
+    return fig, summary
 
 if __name__ == '__main__':
     app.run_server(debug=True)
