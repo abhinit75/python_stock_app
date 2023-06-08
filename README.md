42ad4af51732b544fede161a8a670ba3e2d5c5ee
Abhinit Patil <abhineet0309@gmail.com>
2023-06-07 23:28:57 -0400

    Stock app

--- README.md
+++ README.md
@@ -2,4 +2,18 @@
 
 The aim of this project would be to predict the future values of specific stocks using machine learning, display the predicted values and historical stock data visually.
 
-Additonally, we can make use of Open API to provide data summary to make it more human readable.
+Additonally, we can provide data summary to make it more human readable.
+
+Upcoming Features:
+
+1. User Authentication and Profiles: Implement user registration and authentication. This would allow users to save their preferred stocks, view their search history, and customize their dashboard. Flask-Dash offers integration with Flask-Login which can be used for this purpose.
+
+2. Advanced Financial Metrics: You could display more financial indicators and metrics, such as P/E ratio, EPS, market cap, etc. This would add depth to the analysis you're providing.
+
+3. Stock Comparison: Allow users to compare multiple stocks on the same graph. This would be particularly helpful for users looking to diversify their portfolio.
+
+4. Interactive Charts: Enhance your data visualization with more interactive Plotly graphs. For instance, you could use candlestick charts for stock prices, which provides more information than a simple line plot.
+
+5. Automated Reports: Give users the option to receive daily/weekly/monthly reports via email about their chosen stocks.
+
+6. Real-time Data: Instead of making predictions on historical data only, provide real-time data and forecasts if possible.
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import dash
 from dash.dependencies import Input, Output, State
+from datetime import datetime
 import openai
 import pandas as pd
 import yfinance as yf
@@ -20,16 +21,23 @@ api_key = os.getenv("OPENAPI_KEY")
 openai.api_key = api_key
 
 # Step 1: Gather Data
+
+
 def get_stock_data(ticker):
-    data = yf.download(ticker, '2020-01-01', '2023-12-31')
+    data = yf.download(ticker, '2020-01-01',
+                       datetime.today().strftime('%Y-%m-%d'))
     return data
 
 # Step 2: Feature Engineering
+
+
 def calculate_rolling_average(data):
     data['Rolling'] = data['Close'].rolling(window=5).mean()
     return data
 
 # Step 3: Simple ML model
+
+
 def train_linear_regression_model(data):
     # Drop any rows with missing values
     data.dropna(inplace=True)
@@ -41,39 +49,46 @@ def train_linear_regression_model(data):
     y = data['Close'].shift(-1)
 
     # Split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X[:-1], y[:-1], test_size=0.2, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X[:-1], y[:-1], test_size=0.2, random_state=0)
 
     # Create and fit the model
     model = LinearRegression()
     model.fit(X_train, y_train)
 
-    # Now we can make predictions on the test set
-    predictions = model.predict(X_test)
-
     return model
+
 
 # Step 4: Build the dashboard
 app = dash.Dash(__name__)
 
 # Prepare data for Prophet
+
+
 def prepare_prophet_data(data):
     prophet_data = data[['Close']].reset_index()
     prophet_data.columns = ['ds', 'y']
     return prophet_data
 
 # Instantiate a new Prophet object
+
+
 def fit_prophet_model(data):
     m = Prophet(daily_seasonality=True)
     m.fit(data)
     return m
 
 # Create a dataframe for future predictions
+
+
 def make_prophet_forecast(model):
-    future = model.make_future_dataframe(periods=60)
+    future = model.make_future_dataframe(periods=120)
     forecast = model.predict(future)
     return forecast
 
 # Set plot title and labels
+
+
 def set_plot_layout(fig):
     fig.update_layout(
         title_text='Stock Price Forecast',
@@ -83,6 +98,8 @@ def set_plot_layout(fig):
     )
 
 # Generate the summary after making the prediction
+
+
 def generate_summary(data, prediction):
     message = f"""
     The stock had a closing price of ${round(data['Close'].iloc[-1], 2)} today. 
@@ -91,13 +108,27 @@ def generate_summary(data, prediction):
     """
     return message
 
+
 # Define Dash application layout
 app.layout = html.Div([
-    dcc.Input(id='ticker-input', type='text', value='AAPL', placeholder='Enter stock ticker'),
-    html.Button(id='submit-button', children='Submit', n_clicks=0),
+    html.Div(style={'height': '50px'}),  # Add some vertical space
+    html.Div(
+        html.H2("Enter the ticker symbol below"),
+        style={'textAlign': 'center'}  # Center the text header
+    ),
+    html.Div(
+        dcc.Input(id='ticker-input', type='text', value='AAPL',
+                  placeholder='Enter stock ticker'),
+        style={'textAlign': 'center'}  # Center the input box
+    ),
+    html.Div(
+        html.Button(id='submit-button', children='Submit', n_clicks=0),
+        style={'textAlign': 'center'}  # Center the button
+    ),
     dcc.Graph(id='graph'),
     html.Div(id='summary', style={'textAlign': 'center'})
 ])
+
 
 @app.callback(
     [Output('graph', 'figure'), Output('summary', 'children')],
@@ -123,10 +154,12 @@ def update_output(n_clicks, ticker):
     fig = go.Figure()
 
     # Add actual data to the plot
-    fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Actual'))
+    fig.add_trace(go.Scatter(
+        x=data.index, y=data['Close'], mode='lines', name='Actual'))
 
     # Add forecast data to the plot
-    fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat'], mode='lines', name='Forecast'))
+    fig.add_trace(go.Scatter(
+        x=forecast['ds'], y=forecast['yhat'], mode='lines', name='Forecast'))
 
     # Add confidence interval
     fig.add_trace(go.Scatter(
@@ -152,9 +185,11 @@ def update_output(n_clicks, ticker):
     set_plot_layout(fig)
 
     # Generate the summary after making the prediction
-    summary = generate_summary(data, model.predict([[data['Rolling'].iloc[-1]]])[0])
+    summary = generate_summary(
+        data, model.predict([[data['Rolling'].iloc[-1]]])[0])
 
     return fig, summary
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
